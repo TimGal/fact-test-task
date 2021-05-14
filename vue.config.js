6346e2643f3a35112be044e1b32f3f069ed595f9
Timur Galdin <galdin.timur508@gmail.com>
2021-05-14 15:47:18 +0300

    update link at vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/medods-simon/' : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/fact-test-task/' : '/',
 };
